6604ca3f743b66c6e4e2ca8f06e9553ac7ad4b28
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-02-10 07:45:19 +0500

    added user input

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 /* eslint-disable no-use-before-define */
 import fetch from 'node-fetch';
 
@@ -5,10 +6,43 @@ import xlsx from 'xlsx';
 
 import dotenv from 'dotenv';
 
+import inquirer from 'inquirer';
+
 dotenv.config();
 
-const search = 'q=dogs&num=100';
+let searchName;
+let searchNumber; 
 
+async function searchNameGetter() {
+  const answers = await inquirer.prompt({
+    name: 'search_name',
+    type: 'input',
+    message: 'What do you want to search in google?', 
+    default() {
+      return 'Search';
+    },
+  });
+  searchName = answers.search_name;
+}
+
+async function searchNumberGetter() {
+  const answers = await inquirer.prompt({
+    name: 'search_number',
+    type: 'input',
+    message: 'How many search results do you want? (max 100)', 
+    default() {
+      return 'Number';
+    },
+  });
+  searchNumber = answers.search_number;
+}
+
+await searchNameGetter();
+await searchNumberGetter();
+
+const search = `q=${searchName}&num=${searchNumber}`;
+
+// convert to async await
 fetch(`https://google-search3.p.rapidapi.com/api/v1/search/${search}`, {
   method: 'GET',
   headers: {
@@ -17,24 +51,27 @@ fetch(`https://google-search3.p.rapidapi.com/api/v1/search/${search}`, {
 })
   .then((res) => res.json())
   .then((res) => {
-    console.log(res);
+    // console.log(res);
     dataParsor(res);
   })
   .catch((error) => console.log('error', error));
-
-const fileName = 'GoogleLinks.xlsx';
-const workSheetName = 'results';
-const workBook = xlsx.utils.book_new();
-
+  
+// split into two parts
+const results = [];
 function dataParsor(data) {
-  let content = [['Title', 'Links']];
-
-  data.results.forEach((website, index) => {
-    content[index + 1] = [website.title];
-    content[index + 1].push(website.link); 
+  data.results.forEach((website) => {
+    results.push({
+      'title': website.title,
+      'link': website.link,
+      'description': website.description,
+    });
   });
-  let workSheet = xlsx.utils.aoa_to_sheet(content);
-
-  xlsx.utils.book_append_sheet(workBook, workSheet, workSheetName);
-  xlsx.writeFile(workBook, fileName);
 }
+console.log(results);
+
+// const fileName = 'GoogleLinks.xlsx';
+// const workSheetName = 'results';
+// const workBook = xlsx.utils.book_new();
+// let workSheet = xlsx.utils.aoa_to_sheet(); // array of arrays
+// xlsx.utils.book_append_sheet(workBook, workSheet, workSheetName);
+// xlsx.writeFile(workBook, fileName);
