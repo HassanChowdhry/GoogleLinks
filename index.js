9a2170e91dc3a52eb9d69b705941cbaea97c78b1
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-06-01 12:24:16 +0500

    change to old index file

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* eslint-disable quote-props, no-use-before-define, import/extensions */
-/* eslint-disable import/prefer-default-export */
 import fetch from 'node-fetch';
 import inquirer from 'inquirer';
 import dotenv from 'dotenv';
@@ -12,36 +10,34 @@ dotenv.config();
 
 // Get user input -
 
-// // TODO: validate user input
-// let searchName = (await inquirer.prompt({
-//   name: 'search_name',
-//   type: 'input',
-//   message: 'What do you want to search in google?', 
-// })).search_name;
+// TODO: validate user input
+let searchName = (await inquirer.prompt({
+  name: 'search_name',
+  type: 'input',
+  message: 'What do you want to search in google?', 
+})).search_name;
 
-// // TODO: validate user input
-// let searchNumber = (await inquirer.prompt({
-//   name: 'search_number',
-//   type: 'input',
-//   message: 'How many search results do you want? (max 100)', 
-// })).search_number;
+// TODO: validate user input
+let searchNumber = (await inquirer.prompt({
+  name: 'search_number',
+  type: 'input',
+  message: 'How many search results do you want? (max 100)', 
+})).search_number;
 
-// // Lets do some magic - API call
-// const search = `q=${searchName}&num=${searchNumber}`;
+// Lets do some magic - API call
+const search = `q=${searchName}&num=${searchNumber}`;
 
-export async function createExcel(searchName, searchNumber) {
-  const response = await (await fetch(`https://google-search3.p.rapidapi.com/api/v1/search/q=${searchName}&num=${searchNumber}`, {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': process.env.API_KEY,
-      'x-proxy-location': 'US',
-    },
-  })).json();
+const response = await (await fetch(`https://google-search3.p.rapidapi.com/api/v1/search/q=${searchName}&num=${searchNumber}`, {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': process.env.API_KEY,
+    'x-proxy-location': 'US',
+  },
+})).json();
 
-  // toModel
-  const searchItems = await response.results.map((item) => new SearchItem(item.title, item.description, item.link));
+// toModel
+const searchItems = await response.results.map((item) => new SearchItem(item.title, item.description, item.link));
 
-  // Create excel!
-  const arrayOfArray = excelUtils.toExcelArrayOfArray(searchItems);
-  excelUtils.createExcel(arrayOfArray, searchName);
-}
+// Create excel!
+const arrayOfArray = excelUtils.toExcelArrayOfArray(searchItems);
+excelUtils.createExcel(arrayOfArray, searchName);
