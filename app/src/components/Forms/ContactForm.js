95fb02eece54ea096260366303efb898c6d0c395
Hassan Chowdhry <mohammedchowdhry11@gmail.com>
2022-06-04 14:28:40 +0500

    contact form updte

--- app/src/components/Forms/ContactForm.js
+++ app/src/components/Forms/ContactForm.js
@@ -10,17 +10,30 @@ function ContactForm() {
   const emailInputRef = useRef();
   const subjectInputRef = useRef();
   const messageInputRef = useRef();
-  
+
   const sendEmail = (event) => {
     event.preventDefault();
+    let emailVerificationRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    let name = nameInputRef.current.value.trim();
+    let email = emailInputRef.current.value.trim(); 
+    let subject = subjectInputRef.current.value.trim(); 
+    let message = messageInputRef.current.value.trim();
+  
+    if (name.length > 0 && email.test(emailVerificationRegex) && subject.length > 0 && message.length > 0) {
+      try {
 
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, event.target, process.env.REACT_APP_PUBLIC_KEY)
-    .then((result) => {
-        console.log(result.text);
-    }, (error) => {
-        console.log(error.text);
-    });
-    // event.target.reset();
+      } catch {
+        
+      }
+      // emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, event.target, process.env.REACT_APP_PUBLIC_KEY)
+      // .then((result) => {
+      //     console.log(result.text);
+      // }, (error) => {
+      //     console.log(error.text);
+      // });
+      // event.target.reset();
+
+    }
   }
   
   return (
